8b4b25d4f89bf98a63620f0365efd7b8b67eb943
Michael Wood <michaelwood33311@icloud.com>
2024-05-18 10:56:16 -0400

    assigned components to Acurai page

--- src/pages/Acurai.jsx
+++ src/pages/Acurai.jsx
@@ -1,9 +1,26 @@
 import './Acurai.scss';
 import React from 'react'
+import AdminControls from '../components/AdminControls';
+import ModelSelector from '../components/ModelSelector';
+import LabelsSelector from '../components/LabelsSelector';
+import RAGTruthViewer from '../components/RAGTruthViewer';
+import RAGFixViewer from '../components/RAGFixViewer';
+import GroundTruthViewer from '../components/GroundTruthViewer';
 
 function Acurai() {
   return (
-    <div className='Acurai'>Acurai</div>
+    <div className='Acurai'>
+      <h1 className="Acurai__title">Acurai</h1>
+      <AdminControls />
+      <ModelSelector />
+      <LabelsSelector />
+      <h2 className="Acurai__subtitle">Response ID: </h2>      
+      <div className="Acurai__responses-container">
+        <RAGTruthViewer />
+        <RAGFixViewer />
+      </div>
+      <GroundTruthViewer />
+    </div>
   )
 }
 
