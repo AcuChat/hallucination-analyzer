b590598fd9877d2e71950a40d1efb04905548cad
Michael Wood <michaelwood33311@icloud.com>
2024-05-18 11:15:03 -0400

    Styled admin controls

--- src/components/AdminControls.jsx
+++ src/components/AdminControls.jsx
@@ -6,11 +6,11 @@ function AdminControls() {
     <div className='AdminControls'>
       <div className="AdminControls__submit-button">Submit</div>
       <div className="AdminControls__highlight-container">
+        <div className="AdminControls__reset-button">Reset</div>
         <div className="AdminControls__checkbox-container">
           <input type="checkbox" className="AdminControls__checkbox" />
           <div className="AdminControls__checkbox-label">Highlight</div>
         </div>
-        <div className="AdminControls__reset-button">Reset</div>
       </div>
     </div>
   )
