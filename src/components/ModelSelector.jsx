5aa309af502c0ce4f2d903548cf8e37984d986f5
Michael Wood <michaelwood33311@icloud.com>
2024-05-18 11:26:07 -0400

    added model selection functionality

--- src/components/ModelSelector.jsx
+++ src/components/ModelSelector.jsx
@@ -1,10 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './ModelSelector.scss';
 
-import React from 'react'
+import React from 'react';
+import { modelsSetModel } from '../store/sliceModels';
+
+
 
 function ModelSelector() {
+  const models = useSelector(state => state.models);
+  console.log('models', models)
+  const dispatch = useDispatch();
   return (
-    <div className='ModelSelector'>ModelSelector</div>
+    <div className='ModelSelector'>
+      <select name="" id="" value={models.curModel} onChange={(e) => dispatch(modelsSetModel(e.target.value))}>
+        {models?.availableModels?.map(model => {
+          return (
+            <option key={model} value={model}>{model}</option>
+          )
+        })}
+      </select>
+
+    </div>
   )
 }
 
