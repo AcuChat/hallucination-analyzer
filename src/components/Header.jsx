c4102430e488393f1b7d2b009c2ea9bba6413e53
Michael Wood <michaelwood33311@icloud.com>
2024-05-18 10:44:05 -0400

    finished header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.scss';
-import React from 'react'
+import React from 'react';
+import RAGTruthIcon from '../assets/images/RAGTruth.svg'
 
 function Header() {
   return (
-    <div className='Header'>Header</div>
+    <div className='Header'>
+      <img className='Header__icon' src={RAGTruthIcon} alt="" />
+      <h1 className="Header__title">Hallucination Analyzer</h1>
+    </div>
   )
 }
 
