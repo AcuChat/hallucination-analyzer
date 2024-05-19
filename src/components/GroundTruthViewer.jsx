0c539533f2bae748d726e1f80063a5b335e45f02
Michael Wood <michaelwood33311@icloud.com>
2024-05-19 08:48:59 -0400

    added hiding GroundTruthViewer when no responses

--- src/components/GroundTruthViewer.jsx
+++ src/components/GroundTruthViewer.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux';
 import './GroundTruthViewer.scss';
 import React from 'react'
 
 function GroundTruthViewer() {
+  const responses = useSelector(state => state.responses);
+  if (responses.currentResponseIndex === -1 ) return (<></>)
+  
   return (
     <div className='GroundTruthViewer'>GroundTruthViewer</div>
   )
