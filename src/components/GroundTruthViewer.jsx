177663780a90e5346fe1612daccaeadc04a904f7
Michael Wood <michaelwood33311@icloud.com>
2024-05-19 10:37:51 -0400

    added displaying ground truth

--- src/components/GroundTruthViewer.jsx
+++ src/components/GroundTruthViewer.jsx
@@ -4,10 +4,22 @@ import React from 'react'
 
 function GroundTruthViewer() {
   const responses = useSelector(state => state.responses);
-  if (responses.currentResponseIndex === -1 ) return (<></>)
+  const groundTruth = useSelector(state => state.groundTruth);
+
+  if (responses.currentResponseIndex === -1 || groundTruth.index === -1) return (<></>)
   
   return (
-    <div className='GroundTruthViewer'>GroundTruthViewer</div>
+    <div className='GroundTruthViewer'>
+      <h1 className='GroundTruthViewer__title'>Ground Truth</h1>
+      <div className="GroundTruthViewer__passages">
+        {groundTruth.passages.map(passage => {
+          const key = groundTruth.source + groundTruth.index;
+          return (
+            <div className="GroundTruthViewer__passage" key={key}>{passage}</div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
