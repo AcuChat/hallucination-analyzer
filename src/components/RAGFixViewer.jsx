8cfbe158d95988308af082c94c18ddeb94c513de
Michael Wood <michaelwood33311@icloud.com>
2024-05-19 08:48:31 -0400

    added hiding RAGFixViewer when no responses

--- src/components/RAGFixViewer.jsx
+++ src/components/RAGFixViewer.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux';
 import './RAGFixViewer.scss';
 import React from 'react'
 
 function RAGFixViewer() {
+  const responses = useSelector(state => state.responses);
+  if (responses.currentResponseIndex === -1 ) return (<></>)
+  
   return (
     <div className='RAGFixViewer'>RAGFixViewer</div>
   )
