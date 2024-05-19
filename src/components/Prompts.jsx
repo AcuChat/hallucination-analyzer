06fb225cffd10eda7ef3aa3e0047e0b1121bd078
Michael Wood <michaelwood33311@icloud.com>
2024-05-19 09:29:44 -0400

    added triggering responses fetching when either models or labels changes

--- src/components/Prompts.jsx
+++ src/components/Prompts.jsx
@@ -1,11 +1,26 @@
 import { useSelector } from 'react-redux';
 import './Prompts.scss';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Prompts() {
   const responses = useSelector(state => state.responses);
-  if (responses.currentResponseIndex === -1 ) return (<></>)
+  const models = useSelector(state => state.models);
+  const labels = useSelector(state => state.labels);
   
+  const getResponses = async () => {
+    // const request = {
+    //   url: `https://ragtruth-processor.acur.ai/`,
+    //   method
+    // }
+
+      alert('get responses');
+  }
+
+  useEffect(() => {
+    getResponses();
+  }, [models, labels])
+  
+  if (responses.currentResponseIndex === -1 ) return (<></>)
   return (
     <div className='Prompts'>Prompts</div>
   )
